10236e2f26e99afd1d089a06706f4d706ef0dd76
hongymagic <d.hong@me.com>
2014-06-12 09:47:56 +1000

    Add list command in the toolbar

--- public/js/commands.js
+++ public/js/commands.js
@@ -1,37 +1,79 @@
 (function (window) {
 	"use strict";
 
-// A simple helper function to insert prefix + postfix in the selection range
+	// Return lines in the selection. Assumes \n as the newline character,
+	// however, in unfiltered (i.e., native) contentEditables – the chances
+	// of <br /> tags being used to represent newlines is not uncommon.
+	function getLines(selection) {
+		var lines = selection.toString();
+		if (!lines) {
+			return [];
+		}
+		return lines.split('\n');
+	}
 
-	function surroundWith(document, selection, prefix, postfix) {
+	// Prefix all given lines
+	function prefixLines(lines, prefix) {
+		return lines.map(function (line) {
+			return prefix + line;
+		});
+	}
 
-// If no document is given, use the default window.document
+	// A simple helper function to remove currently selected contents and
+	// replace it with lines
+	function replaceWithLines(document, selection, lines) {
 
 		if (document === null || document === undefined) {
 			document = window.document;
 		}
 
-// If no selection is made, nothing to do
-
 		if (selection.rangeCount === 0) {
 			return;
 		}
 
-// If postfix is not given, let prefix == postfix
+		var range = selection.getRangeAt(0);
+		range.deleteContents();
+		range.collapse(false);
 
+		if (!lines) {
+			return;
+		}
+
+		var fragment = document.createDocumentFragment();
+		lines.forEach(function (line) {
+			fragment.appendChild(document.createTextNode(line));
+			fragment.appendChild(document.createElement('br'));
+		});
+
+		range.insertNode(fragment.cloneNode(true));
+	}
+
+	// A simple helper function to insert prefix + postfix in the selection
+	// range
+	function surroundWith(document, selection, prefix, postfix) {
+
+		// If no document is given, use the default window.document
+		if (document === null || document === undefined) {
+			document = window.document;
+		}
+
+		// If no selection is made, nothing to do
+		if (selection.rangeCount === 0) {
+			return;
+		}
+
+		// If postfix is not given, let prefix == postfix
 		if (!postfix) {
 			postfix = prefix;
 		}
 
-// Insert the prefix
-
+		// Insert the prefix
 		var range = selection.getRangeAt(0);
 		range.insertNode(document.createTextNode(prefix));
 
 		range.collapse(false);
 
-// And the postfix
-
+		// And the postfix
 		selection.removeAllRanges();
 		selection.addRange(range);
 		range.insertNode(document.createTextNode(postfix));
@@ -54,6 +96,12 @@
 
 		code: function (editor, selection) {
 			surroundWith(editor.editorIframeDocument, selection, '`');
+		},
+
+		list: function (editor, selection) {
+			debugger;
+			var list = prefixLines(getLines(selection), '\t - ');
+			replaceWithLines(editor.editorIframeDocument, selection, list);
 		}
 	};
 
